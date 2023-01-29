8d7b34a9be6c569adb6a6cc0e393e06e3bb58856
ADMIN <hiddenwory@gmail.com>
2023-01-29 21:33:46 +0700

    Invoice Detail

---
+++ BillProcessingApp/Form/FrmInvoiceDetail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BillProcessingApp
+{
+    public partial class FrmInvoiceDetail : Form
+    {
+        public FrmInvoiceDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void lbCreateDate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- BillProcessingApp/Program.cs
+++ BillProcessingApp/Program.cs
@@ -17,7 +17,7 @@ namespace BillProcessingApp
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmInvoiceDetail());
         }
     }
 }
